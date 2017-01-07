d5b52e0bd3b3db7edb6015004ad623dcdb410d73
Zak <zpsaras@mit.edu>
2017-01-07 18:41:36 -0500

    Board wrapper

---
+++ PBots/src/Board.py
@@ -0,0 +1,41 @@
+from deuces import Deck
+from deuces import Card
+from deuces import Evaluator
+
+class Board:
+    board_cards = []
+    my_hand = None
+    deck = Deck()
+    
+    #Both player hand and flop cards should be in the 
+    # form of strings
+    def __init__(self, player_hand_string, flop=False):
+        self.my_hand = [Card.new(i) for i in player_hand_string]
+        
+        self.deck.cards.remove(self.my_hand[0])
+        self.deck.cards.remove(self.my_hand[1])
+        print len(self.deck.cards)
+        if flop:
+            for new_card in flop:
+                self.board_cards.append(Card.new(new_card))
+                self.deck.cards.remove(self.board_cards[-1])
+        
+        print self.my_hand
+        print self.board_cards
+        print len(self.deck.cards)
+        
+    def add(self, new_card):
+        self.board_cards.append(Card.new(new_card))
+        self.deck.cards.remove(self.board_cards[-1])
+    
+    def fillBoardRandomly(self):
+        while len(self.board_cards) != 5:
+            self.board_cards.append(self.deck.draw())
+          
+    def printBoard(self):
+        print "My Hand:  "
+        Card.print_pretty_cards(self.my_hand)
+        print "Board:  "
+        Card.print_pretty_cards(self.board_cards)
+          
+
--- PBots/src/main.py
+++ PBots/src/main.py
@@ -1,6 +1,17 @@
-import math
+import Board
 
-def main():
-    return "pussyballs"
+from deuces import Deck
+from deuces import Evaluator
+from deuces import Card
 
-print main()
+evalutor = Evaluator()
+deck = Deck()
+p1 = ['Th','9d']
+c = ['6c', '7d','2c']
+
+b = Board.Board(p1, c)
+print evalutor.evaluate(b.my_hand, b.board_cards)
+b.printBoard()
+b.add(Card.int_to_str(deck.draw()))
+print evalutor.evaluate(b.my_hand, b.board_cards)
+b.printBoard()
